16c0cfd0666d2a6cba9852066b37acafc7b782d7
Sanil Shrestha <sanilshrestha@lftechnology.com>
2014-01-25 20:21:28 +0545

    ROR self study CRUD project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crud::Application.config.secret_key_base = 'f9947d1fface1cfde1c804c1f0bb6d62045b6eaec2874e119a513c5772b44f5a0de7395f5333c5a0d28ba868715d2ddd1ebf294bf4b209768742003eabc2b56e'
